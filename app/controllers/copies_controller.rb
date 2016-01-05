1435be5e6b2ef9fba3601cb7daac73adcf622df4
ChrisS-ed <c.sloane@hotmail.co.uk>
2016-01-05 15:55:36 +0000

    added associations

---
+++ app/controllers/copies_controller.rb
@@ -0,0 +1,34 @@
+class CopiesController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+    
+  end
+
+  def show
+      
+    end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  def edit
+    
+  end
+
+  def update
+
+  end
+
+  private
+  def copy_params
+   params.require(:copy).permit(:game_id)
+ end
+
+end
--- app/models/copy.rb
+++ app/models/copy.rb
@@ -1,2 +1,4 @@
 class Copy < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :game
 end
